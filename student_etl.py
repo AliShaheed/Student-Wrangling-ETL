9b56eda25e013ef576f2095aa634a3660bebe0b1
AliShaheed <alishaheed1138@gmail.com>
2022-07-24 21:07:22 +0100

    added functions to format birth_date

--- student_etl.py
+++ student_etl.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from datetime import datetime
 
 def drop_columns(data_frame):
     data_frame = data_frame.drop(columns=['Zscore',
@@ -20,6 +20,22 @@ def remove_absentee(data_frame):
                            ].index)
     return absent_df
 
+def remove_invalid_days(data_frame):
+    data_frame = data_frame.drop(data_frame[(data_frame.birth_day == 'Invalid error') | (data_frame.birth_day == 'Invalid')].index)
+    data_frame['birth_day'] = data_frame['birth_day'].convert_dtypes()
+    data_frame['birth_day'] = pd.to_numeric(data_frame['birth_day'])
+    data_frame = data_frame.drop(data_frame[(data_frame.birth_day > 31) | (data_frame.birth_day < 1)].index)
+    return data_frame
+
+def merge_birthdate(data_frame):
+    data_frame = remove_invalid_days(data_frame) # check if number of days is out of range, if true remove them.
+    data_frame['birth_date'] = data_frame['birth_day'].astype(str) + " " + (data_frame['birth_month'].astype(str) + " " + data_frame['birth_year'])
+    data_frame = data_frame.drop(columns=['birth_day',
+                                          'birth_month',
+                                          'birth_year'],
+        axis=1)
+
+    return data_frame
 
 def extract(file):
     data_frame = pd.read_csv(file)
@@ -28,7 +44,8 @@ def extract(file):
 def transform(data_frame):
     dropped_df = drop_columns(data_frame)
     missing_df = remove_missing(dropped_df)
-    trans_df = remove_absentee(missing_df)
+    not_absent_df = remove_absentee(missing_df)
+    trans_df = merge_birthdate(not_absent_df)
     return trans_df
 
 csv_file = "al_results_2020.csv"

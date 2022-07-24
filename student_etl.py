6ff42e4eaeedce00308a51caa124994cc6730967
AliShaheed <alishaheed1138@gmail.com>
2022-07-24 16:11:00 +0100

    added transform and drop_columns function

--- student_etl.py
+++ student_etl.py
@@ -1,11 +1,23 @@
 import pandas as pd
 
+
+def drop_columns(data_frame):
+    data_frame = data_frame.drop(columns=['Zscore',
+                                          'gender',
+                                          'syllabus'],
+        axis=1)
+    return data_frame
+
 def extract(file):
-    data = pd.read_csv(file)
-    return data
-    
+    data_frame = pd.read_csv(file)
+    return data_frame
 
-file = "al_results_2020.csv"
-df = extract(file)
+def transform(data_frame):
+    trans_df = drop_columns(data_frame)
+    return trans_df
 
-print(df.head())
+csv_file = "al_results_2020.csv"
+df = extract(csv_file)
+trans_df = transform(df)
+
+print(trans_df.head())

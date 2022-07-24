178afd2dc758712cf62eb504ac030819a1d47bfd
AliShaheed <alishaheed1138@gmail.com>
2022-07-24 16:44:30 +0100

    added remove_missing function

--- student_etl.py
+++ student_etl.py
@@ -8,16 +8,22 @@ def drop_columns(data_frame):
         axis=1)
     return data_frame
 
+def remove_missing(data_frame):
+    missing_df = data_frame.dropna()
+    return missing_df
+
 def extract(file):
     data_frame = pd.read_csv(file)
     return data_frame
 
 def transform(data_frame):
-    trans_df = drop_columns(data_frame)
+    dropped_df = drop_columns(data_frame)
+    trans_df = remove_missing(dropped_df)
     return trans_df
 
 csv_file = "al_results_2020.csv"
 df = extract(csv_file)
 trans_df = transform(df)
 
-print(trans_df.head())
+
+print(trans_df)
